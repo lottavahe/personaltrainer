c8da8afd8b460beaa0914facc4ec4fe011c09c90
lottavahe <lotta.vahe@gmail.com>
2026-04-26 13:58:19 +0300

    korjasin routingin github pagesia varten

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import CalendarPage from './pages/CalendarPage.tsx'
 import CustomersPage from './pages/CustomersPage.tsx'
 import TrainingsPage from './pages/TrainingsPage.tsx'
 
 //tämä kertoo reactroutterille millä url osoitteella 
 //pitää näyttää mitäkin sivua, hoitaa sivujen vaihtamisen
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <App />,
